f9af5172a9003744d91f736531557a15c5800cd2
BhupeshChikara <bhupesh.chikara@tothenew.com>
2020-04-06 22:50:35 +0530

    http unsubscribe

--- src/app/https/https.component.ts
+++ src/app/https/https.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-https',
@@ -8,16 +9,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpsComponent implements OnInit {
   data;
+  subscription: Subscription;
 
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://jsonplaceholder.typicode.com/posts/1/comments').subscribe(res=>{
+    this.subscription=this.http.get('https://jsonplaceholder.typicode.com/posts/1/comments').subscribe(res=>{
       console.log(res)
       this.data=res;
     },err=>{
       console.log(err)
     })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
